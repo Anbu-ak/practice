b0da71c4c6c6b55167b53de65d023f8c11dffa7d
Anbu-ak <anbuiniyanias2000@gmail.com>
2023-11-28 12:05:52 +0530

    practice 1

---
+++ com/company/set.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+import java.util.HashSet;
+import  java.util.Set;
+import java.util.HashMap;
+
+
+public class set {
+    public static void main(String[] args) {
+      /*  Set se=new HashSet();
+        se.add("raja");
+        se.add(1);
+        se.add(23);
+        se.add(35);
+        se.add("raja");
+        System.out.println(se);*/
+
+    }
+}
